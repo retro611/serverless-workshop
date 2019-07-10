16aa4553e904f1b60591c7883c21c203f258a5a4
retro611 <retro611@gmail.com>
2019-07-10 10:47:57 +0800

    configured cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: ' ap-southeast-1_eSSTYyBj4', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7ifcpanomjg7md9fcmvvva8tmh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
