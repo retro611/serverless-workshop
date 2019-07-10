1d0f2de53543077dc3078df4d60c29224cf0119e
retro611 <retro611@gmail.com>
2019-07-10 11:00:06 +0800

    updated cognito

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: ' ap-southeast-1_eSSTYyBj4', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'ap-southeast-1_eSSTYyBj4', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '7ifcpanomjg7md9fcmvvva8tmh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-1' // e.g. us-east-2
     },
